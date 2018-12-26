b91244e11c4b57b42503ee82342b34f8d1702d88
GuangWei Li <liguwe832@gmail.com>
2018-12-26 10:51:01 +0800

    workflow: update .gitignore

--- app/navigation/NavigationStack.js
+++ app/navigation/NavigationStack.js
@@ -34,9 +34,9 @@ const RNApp = createStackNavigator(
             //     gesturesEnabled: false
             // }
         },
-        Mine : {
-            screen: Publish,
-        }
+        // Mine : {
+        //     screen: Publish,
+        // }
     },
 
     {
--- app/utils/stringUtils.js
+++
@@ -1,8 +0,0 @@
-export const getInitials = name => {
-    let initials = Array.prototype.map
-        .call(name.split(' '), function(x) {
-            return x.substring(0, 1).toUpperCase();
-        })
-        .join('');
-    return initials.substring(0, 2);
-};
