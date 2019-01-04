1db69e2e1e4352571dd2b3357d422f85b3837be5
Mr-jiangzhiguo <jiangzhiguo2010@qq.com>
2019-01-04 13:01:42 +0800

    workflow(disclose): 爆料列表页更新

--- app/screens/Disclose/List/Container.js
+++ app/screens/Disclose/List/Container.js
@@ -66,6 +66,54 @@ function mapStateToProps() {
                         uri: 'http://img.hb.aicdn.com/90f0e0780b767a82885bafdf9526ee0224642f803befd8-qDYNht_fw658'
                     }
                 ]
+            },
+            {
+                title: '하지만 스마트폰이 우리 삶에 깊게 들어온 만큼, 그로 인한 어두운 면 역시 부각되고 있습니다.',
+                time: moment().format("mm:ss"),
+                userName: '匿名',
+                source: source,
+                num1: 1234,
+                num2: 1234,
+                num3: 1234,
+                num4: 1234,
+                images: [
+                    {
+                        uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png',
+                    },
+                    {
+                        uri: 'http://img.hb.aicdn.com/3f488166d89c1c933e01efb3c7b1ba8d03c1f467c6239-8QXmaI_fw658',
+                    },
+                    {
+                        uri: 'http://img.hb.aicdn.com/2ef0972e721f55fa0b176eca9c56c1909cf86c41496141-z4MJmr_fw658'
+                    },
+                    {
+                        uri: 'http://img.hb.aicdn.com/90f0e0780b767a82885bafdf9526ee0224642f803befd8-qDYNht_fw658'
+                    }
+                ]
+            },
+            {
+                title: '하지만 스마트폰이 우리 삶에 깊게 들어온 만큼, 그로 인한 어두운 면 역시 부각되고 있습니다.',
+                time: moment().format("mm:ss"),
+                userName: '匿名',
+                source: source,
+                num1: 1234,
+                num2: 1234,
+                num3: 1234,
+                num4: 1234,
+                images: [
+                    {
+                        uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png',
+                    },
+                    {
+                        uri: 'http://img.hb.aicdn.com/3f488166d89c1c933e01efb3c7b1ba8d03c1f467c6239-8QXmaI_fw658',
+                    },
+                    {
+                        uri: 'http://img.hb.aicdn.com/2ef0972e721f55fa0b176eca9c56c1909cf86c41496141-z4MJmr_fw658'
+                    },
+                    {
+                        uri: 'http://img.hb.aicdn.com/90f0e0780b767a82885bafdf9526ee0224642f803befd8-qDYNht_fw658'
+                    }
+                ]
             }
         ]
     };
--- app/screens/Disclose/List/styles.js
+++ app/screens/Disclose/List/styles.js
@@ -1,6 +1,7 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+    
     container: {
         flex: 1,
         justifyContent: 'center',
@@ -24,6 +25,44 @@ const styles = StyleSheet.create({
         height: 100,
         borderRadius: 8,
         // resizeMode: 'cover'
+    },
+
+    name: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    time: {
+        color: '#666',
+        fontSize: 12,
+        marginLeft: -6,
+        paddingLeft: 0,
+        // alignSelf: 'baseline',
+    },
+    edit: {
+        textAlign: 'right'
+    },
+    interact: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    number: {
+        color: '#666',
+        fontSize: 12,
+        paddingLeft: 3,
+        marginLeft: 0,
+    },
+    loadMore: {
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    noData: {
+        paddingTop: 10,
+        paddingBottom: 10,
+        textAlign: 'center',
     }
 });
 
