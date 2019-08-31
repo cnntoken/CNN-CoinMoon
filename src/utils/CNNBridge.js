1293066c44c059b32d7a00b78a664d407ff9434d
mlqgg <maliquan@hinterstellar.com>
2019-08-31 19:04:20 +0800

    fix: add utils && fix bridge

--- src/utils/CNNBridge.js
+++ src/utils/CNNBridge.js
@@ -1,15 +1,24 @@
-import {NativeModules} from 'react-native';
-import discloseName from "@services/discloseName";
+import {NativeModules, Platform,NativeEventEmitter} from 'react-native';
 import {adaptUserInfo} from "@utils";
-
-const {CNNRNBridgeManage} = NativeModules;
-
+const {CNNRNBridgeManage,NativeLogger,CNNRNEventManage} = NativeModules;
+// console.log('=============CNNRNEventManage=============',CNNRNEventManage)
 /**
  * 关闭当前rn页面
  */
-export const closeRNPage = async () => {
-    CNNRNBridgeManage.closeRNPage({});
-}
+export const closeRNPage = async (obj) => {
+    await CNNRNBridgeManage.closeRNPage(obj || {});
+};
+
+
+/**
+ * 设置statusbar的背景色
+ */
+export const setStatusColor = async (color='#ffffff') => {
+    if(Platform.OS === 'ios'){
+        await CNNRNBridgeManage.changeStatusColor(color);
+    }
+};
+
 
 /**
  * 跳转到其他rn页面
@@ -18,20 +27,69 @@ export const closeRNPage = async () => {
  *  params: {} // 传参是一个字典, 在目标页面在this.props当中接收
  * }
  */
-export const goRNPage = (obj = {}) => {
-    if (!obj.moduleName) {
-        throw Error('moduleName is necessary')
+export const goRNPage = (()=>{
+    let prevModuleName = '';
+    let time = Date.now();
+    return (obj = {}) => {
+        if (!obj.moduleName) {
+            throw Error('moduleName is necessary')
+        }
+        const newTime = Date.now();
+        const differ = newTime - time;
+        if(obj.moduleName === prevModuleName && differ < 500){
+            return false;
+        }
+        prevModuleName = obj.moduleName;
+        time = newTime;
+        if (!obj.params) {
+            obj.params = {}
+        }
+        CNNRNBridgeManage.goRNPage(obj);
     }
-    if (!obj.params) {
-        obj.params = {}
-    }
-    CNNRNBridgeManage.goRNPage(obj);
-};
+})();
 
+/**
+ * 跳转native页面
+ * @param {Object} obj = {
+ *  moduleName: 'stark_login' // native页面名
+ *  params: {} // 传参是一个字典, 在目标页面使用
+ * }
+ */
+export const goNativePage = (()=>{
+    let prevModuleName = '';
+    let time = Date.now();
+    return (obj = {}) => {
+        if (!obj.moduleName) {
+            throw Error('moduleName is necessary')
+        }
+        const newTime = Date.now();
+        const differ = newTime - time;
+        if(obj.moduleName === prevModuleName && differ < 500){
+            return false;
+        }
+        prevModuleName = obj.moduleName;
+        time = newTime;
+        if (!obj.params) {
+            obj.params = {}
+        }
+        CNNRNBridgeManage.goNativePage(obj);
+    }
+})();
+
+/**
+ * 用于在RN内部跳转的时候，Android需要知道当前页面是否为root页面
+ * 暂时只有Market的Tab页需要这个额外的处理，其他的页面不需要
+ * @param {*} params 
+ */
+export const isRNRootPage = (params) => {
+    if (Platform.OS === 'android') {
+        CNNRNBridgeManage.isRNRootPage(params);
+    }
+}
 
 /**
  * 显示一个toast
- * @param {String} msg 
+ * @param {String} msg
  */
 export const showToast = async (msg = '') => {
     return await CNNRNBridgeManage.showToast(msg);
@@ -44,15 +102,15 @@ export const deviceInfo = CNNRNBridgeManage.deviceInfo;
 
 /**
  * 向native传递一个事件
- * @param {String} eventName 
- * @param {Object} params 
+ * @param {String} eventName
+ * @param {Object} params
  * params = {
  *  type: '', // 事件类型 如: feed/feed_view_count_add
  *  id: '' // 操作id, feedid或者是discloseid. 如: 5ce77ce00000000000000000
  * }
  */
-export const dispatchEvent = (eventName, params)=>{
-    CNNRNBridgeManage.eventEmit({eventName,params});
+export const dispatchEvent = (eventName, params) => {
+    CNNRNBridgeManage.eventEmit({eventName, params});
 }
 
 
@@ -81,3 +139,12 @@ export const getCurrentUser = async () => {
 export const logout = async () => {
     return await CNNRNBridgeManage.logout({});
 }
+
+
+
+export const log2native = async (eventName, params = {})=>{
+    return NativeLogger.log(eventName, params)
+}
+
+
+export const CNNRNEventEmitter = new NativeEventEmitter(CNNRNEventManage);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,15 @@
-import {showToast, dispatchEvent} from './CNNBridge'
+import {showToast, dispatchEvent, log2native} from './CNNBridge'
 import discloseName from "@src/services/discloseName";
-import {DeviceEventEmitter} from 'react-native';
+import {
+    DeviceEventEmitter,
+    Dimensions,
+    Platform,
+    ActionSheetIOS,
+    NativeModules
+} from 'react-native';
+import {CNNRNEventEmitter} from '@utils/CNNBridge'
 import avatars from '@services/constants';
+
 
 const moment = require('moment');
 
@@ -29,15 +37,17 @@ export function getSeconds(createdAt) {
 }
 
 
-// 根据创建日期获取秒数
+//
 export function getNumByUserId(userId) {
-    let num = 0;
+    // let num = 0;
 
-    String(userId).replace(/[^0-9]/ig, "").split('').forEach((item) => {
-        num += Number(item);
-    });
+    // String(userId).replace(/[^0-9]/ig, "").split('').forEach((item) => {
+    //     num += Number(item);
+    // });
 
-    return num;
+    return  Number(String(userId || 0).split('').pop());
+
+    // return num;
 }
 
 export function formatDate(date) {
@@ -68,7 +78,7 @@ export const debounce = (fn, delay) => {
     }
 };
 
-export const debounce_next = function (params = {}) {
+export const debounce_next = (params = {}) => {
     return function (target, name, descriptor) {
         let timer = null
         const {
@@ -142,10 +152,10 @@ export const numWithUnit = (num = 0) => {
     if (typeof Number(num) === 'number' && !Number.isNaN(num)) {
         if (Number(num) >= 100000000) {
             // 亿
-            return `${(Number(num)/100000000).toFixed(2)} 亿`
-        } else if(Number(num) >= 10000){
+            return `${(Number(num) / 100000000).toFixed(2)}십억`
+        } else if (Number(num) >= 10000) {
             // 万
-            return `${(Number(num)/10000).toFixed(4)} 万`
+            return `${(Number(num) / 10000).toFixed(2)}만`
         } else {
             return formateNum(Number(num))
         }
@@ -158,15 +168,23 @@ export const numWithUnit = (num = 0) => {
 export const formateNum = (num = 0) => {
 
     if (typeof Number(num) === 'number' && !Number.isNaN(num)) {
-        if (Number(num) >= 1000) {
-            return Number(num).toFixed(2)
-        } else if(Number(num) >= 10){
+        if (Number(num) >= 10000000) {
+            // 大于等于一千万，用科学技术法表示，并保留4位小数
+            // return Number(num).toExponential(4)
+            return Number(num).toFixed(0)
+        } else if (Number(num) < 10000000 && Number(num) >= 1000) {
+            return Number(num).toFixed(3)
+        } else if (Number(num) < 1000 && Number(num) >= 10) {
             return Number(num).toFixed(4)
-        } else if(Number(num)<10 && Number(num)>0){
+        } else if (Number(num) < 10 && Number(num) > 1e-5) {
             return Number(num).toFixed(5)
-        } else if(Number(num)===0){
+        } else if (Number(num) <= 1e-5 && Number(num) > 0) {
+            //小于等于0.00001，用科学计数法表示，并保留4位小数
+            // return Number(num).toExponential(4)
+            return Number(num).toFixed(7)
+        } else if (Number(num) === 0) {
             return Number(num).toFixed(2)
-        } else if(Number(num) < 0){
+        } else if (Number(num) < 0) {
             let temp = formateNum(Number(num) * -1)
             return temp * -1
         }
@@ -188,23 +206,47 @@ export const swapReverse = (arr) => {
     }
     return arr
 }
+// url query对象转参数字符串
+export const qsStringify = (obj = {}) => {
+    return Object.keys(obj).filter(k => obj[k] || +obj[k] === 0).map(k => {
+        let value = obj[k];
+        if (typeof value === 'object') {
+            value = encodeURIComponent(JSON.stringify(value))
+        } else {
+            value = encodeURIComponent(value)
+        }
+        return `${encodeURIComponent(k)}=${value}`
+    }).join('&');
+}
+// url query参数字符串转json对象
+export const qsParse = (str) => {
+    const obj = {};
+    str.split('&').forEach(item => {
+        const arr = item.split('=')
+        obj[arr[0]] = decodeURIComponent(arr[1])
+    })
+    return obj;
+}
 
 // 适配用户数据格式
 export const adaptUserInfo = (user) => {
     user = user || {};
     // 为了适配之前的数据格式
     user.picture = user.avatar;
+
+
+    // user.id = user.id ;
     user.id = user.user_id;
 
     if (!user.name) {
         user.discloseName = discloseName(user.user_id);
     }
-    user.discloseName = discloseName(user.user_id);
+    user.discloseName = user.discloseName || discloseName(user.user_id);
 
-    user.nickname = user.name;
+    user.nickname = user.nickname || user.name;
 
     let avatarType = getNumByUserId(user.id);
-    user.disclose_avatar = avatars[avatarType % 5];
+    user.disclose_avatar = user.disclose_avatar || avatars[avatarType % 5];
 
     // 因为目前仅仅支持手机号注册登录
     user.isLogin = user.account_type === "sms" && !!user.token;
@@ -215,10 +257,11 @@ export const adaptUserInfo = (user) => {
 
 
 // 通知原生列表数据改变
-export const NoticeUpdateNativeList = (type, args) => {
+export const NoticeUpdateNativeList = (type, args, cb) => {
     try {
 
         const targetId = args.id ? args.id : args.params ? args.params.id : '';
+
         console.log(`================= CNN_TOPIC_STATUS_CHANGE =================`, {
             type,
             id: targetId,
@@ -230,17 +273,116 @@ export const NoticeUpdateNativeList = (type, args) => {
             params: args.params || args.data || {},
         });
 
+
         dispatchEvent("CNN_TOPIC_STATUS_CHANGE", {type, id: targetId, data: args.data || {}});
+
+        if (cb) cb();
 
     } catch (e) {
         console.log(e);
     }
 };
+
 // market 自选操作在列表页与详情页事件监听
-export const NoticeMarketUpdate = (funcName,args) => {
-    try{
-        DeviceEventEmitter.emit(funcName,args)
-    }catch(e){
+export const NoticeMarketUpdate = (funcName, args) => {
+    try {
+        DeviceEventEmitter.emit(funcName, args)
+    } catch (e) {
         console.log(e);
+    }
+}
+
+export const calculateStyleVariable = () => {
+    // const isIphone = Platform.OS === 'ios'
+    const {width: deviceWidth} = Dimensions.get('window')
+    const scaleVariable = deviceWidth / 375
+    return scaleVariable
+};
+
+
+export const ActionSheet = (options, callback) => {
+
+    if (Platform.OS === 'ios') {
+        ActionSheetIOS.showActionSheetWithOptions(options, callback);
+    } else {
+        NativeModules.ActionSheetAndroid.showActionSheetWithOptions(options.options, options.colors || ['#FF3B30', '#007AFF'], callback);
+    }
+
+};
+
+export const isIOS = Platform.OS === 'ios';
+
+
+export function isIphoneX() {
+    const dimen = Dimensions.get('window');
+    return (
+        Platform.OS === 'ios' &&
+        !Platform.isPad &&
+        !Platform.isTVOS &&
+        ((dimen.height === 812 || dimen.width === 812) || (dimen.height === 896 || dimen.width === 896))
+    );
+}
+
+export const cnnLogger = (...args) => {
+    log2native(...args);
+};
+
+export const splitNum = (num) => {
+    let temp_num = Number(num)
+    if(!isNaN(temp_num)){
+        if(temp_num>10000000){
+            return temp_num.toFixed(0)
+        } else if(temp_num>=10){
+            return temp_num.toFixed(2)
+        } else if(temp_num>=1&&temp_num<10){
+            return temp_num.toFixed(4)
+        } else if(temp_num>0 && temp_num<1){
+            let n =  temp_num.toFixed(8)
+            if(n == 0){
+                //如果8位小数仍然不够，则不做处理直接返回
+                return temp_num
+            } else {
+                return n
+            }
+        } else if(temp_num < 0){
+            let reciprocal = -1 * temp_num
+            return -1 * splitNum(reciprocal)
+        } else {
+            return temp_num
+        }
+    } else {
+        return num
+    }
+}
+
+export const getUserStateChangeEventEmitter = ()=>{
+    if (Platform.OS === 'ios') {
+        return CNNRNEventEmitter;
+    } else {
+        return DeviceEventEmitter;
+    }
+}
+
+export const marketCollectionEventEmitter = () => {
+    if (Platform.OS === 'ios') {
+        return CNNRNEventEmitter;
+    } else {
+        return DeviceEventEmitter;
+    }
+}
+
+export const eventEmitterFromRNSelf = (funcName, args) => {
+    try {
+        DeviceEventEmitter.emit(funcName, args)
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export const appTabChangeEventEmitter = () => {
+    if (Platform.OS === 'ios') {
+        return CNNRNEventEmitter;
+    } else {
+        return DeviceEventEmitter;
     }
 }
